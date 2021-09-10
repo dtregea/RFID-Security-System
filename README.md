2a614d054715cfb667e7141873a9d9bf1303280e
dtregea <daniel.tregea@gmail.com>
2021-09-10 15:54:36 -0400

    Servo Motor functionality implemented
    Button functionality implemented

--- README.md
+++ README.md
@@ -7,7 +7,11 @@ The use of Arduino Uno to simulate a RFID tag based security system.
 - RFID tag
 - External breadboard
 - 2 External LED's
-- 2 resistors
+- 3 Resistors
+- Button
+- Breadboard Power Supply Module
+- 5V Power source for breadboard power supply module
+- Servo Motor
 
 ## Pin Layout
 1. RC522
@@ -20,6 +24,17 @@ The use of Arduino Uno to simulate a RFID tag based security system.
 - RST | Digital pin 9
 - 3.3V | 3.3V (Not 5v)
 
-2. LED on Breadboard
+2. LED's on Breadboard
 - Access Denied LED | Digital pin 8
 - Access Authorized LED | Digital pin 7
+- 2 Transistors
+
+3. Servo Motor on Breadboard
+- PWM | Digital pin 5
+- 5V |  Breadboard Power
+- GND | GND
+
+4. Button on Breadboard
+- 5V | Breadboard Power
+- GND | GND
+- 1 Transistor
--- RFID_Security_System.ino
+++ RFID_Security_System.ino
@@ -2,12 +2,23 @@
 #include <MFRC522.h>
 #include <MFRC522Extended.h>
 #include <require_cpp11.h>
+#include <Servo.h>
 
+// Your tag ID here. See Serial output to see your tag ID to replace.
+const String MY_ID = "A7A5019"; 
 const int pinRST = 9;
 const int pinSDA = 10;
-// Your tag ID here. See later code to see how to find it.
-const String MY_ID = "A7A5019"; 
+const int LOCKED_POSITION = 180;
+const int UNLOCKED_POSITION = 0;
+const int BUTTON_PIN = 2;
+const int AUTH_LED = 7;
+const int DENY_LED = 8;
+int lockState = HIGH;
+int buttonState = LOW;
+
+
 MFRC522 mfrc522(pinSDA, pinRST); // Set up mfrc522 on the Arduino
+Servo lockServo;
 
 void setup() {
   SPI.begin(); // open SPI connection
@@ -15,31 +26,42 @@ void setup() {
   Serial.begin(9600); // open serial connection
   pinMode(7, HIGH);
   pinMode(8, HIGH);
+  pinMode(BUTTON_PIN, INPUT);
+  lockServo.attach(5);
+  lockServo.write(LOCKED_POSITION);
+  
 }
 
 void loop() {
   String readID = "";
-  int led;
   if (mfrc522.PICC_IsNewCardPresent()) { // (true, if RFID tag/card is present ) PICC = Proximity Integrated Circuit Card
     if(mfrc522.PICC_ReadCardSerial()) { // true, if RFID tag/card was read
-      //Serial.print("RFID TAG ID:");
+      Serial.print("RFID Tag: ");
       for (byte i = 0; i < mfrc522.uid.size; ++i) { // read id (in parts)
-        // Uncomment this to read the card number in order to set up which tags and their ID's authorize access
-        //Serial.print(mfrc522.uid.uidByte[i], HEX);
+        Serial.print(mfrc522.uid.uidByte[i], HEX);
         readID.concat(String(mfrc522.uid.uidByte[i], HEX));
       }
       readID.toUpperCase();
-      if(readID.compareTo(MY_ID) == 0){
-          Serial.print("\n" + readID + " Authorized access\n");
-          digitalWrite(7, HIGH);
-          led = 7;
-        } else{
-          Serial.print(readID + " Denied access\n");
-          digitalWrite(8, HIGH);
-          led = 8;
-        }
-       delay(1500);
-       digitalWrite(led, LOW);
+      if(readID.compareTo(MY_ID) == 0){// Authorized, Unlock
+          Serial.print(" Authorized access\n");
+          lockState = LOW;
+          digitalWrite(AUTH_LED, HIGH);
+          lockServo.write(UNLOCKED_POSITION);  
+      } else{ // Denied, Stay locked 
+          Serial.print(" Denied access\n");
+          digitalWrite(DENY_LED, HIGH);
+      }
+      delay(1500);
+      digitalWrite(DENY_LED, LOW);
+    }
+  }
+  // While unlocked, cannot read cards until door is shut (button is pressed)
+  while(lockState == LOW){
+    buttonState = digitalRead(BUTTON_PIN);
+    if(buttonState == HIGH){
+      lockServo.write(LOCKED_POSITION);
+      digitalWrite(AUTH_LED, LOW);
+      lockState = HIGH;
     }
   }
 }
